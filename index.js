d691e758d24145da778a5cd70a3f08b5c42d76cc
keshav9761 <keshavtomar53@gmail.com>
2025-07-07 20:05:43 +0530

    portout

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors())
 
 // http://localhost:4000/api/test-pipeline
 app.get('/api/test-pipeline', (req, res) => {
-    res.send("hello world test pipeline kk")
+    res.send("hello world test pipeline kkk")
 })
 
 app.listen(PORT, (req, res) => {
